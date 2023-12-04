075841b5cd7293b0a09348324746a0e20da1ae41
X00205719 <X00205719@myTUDublin.ie>
2023-12-04 21:57:37 +0000

    feat: add chart

--- BPCalculator/wwwroot/js/site.js
+++ BPCalculator/wwwroot/js/site.js
@@ -1,4 +1,92 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const ctx = document.getElementById('myChart');
+const systolic = document.getElementById("BP_Systolic").value;
+const diastolic = document.getElementById("BP_Diastolic").value;
 
-// Write your Javascript code.
+const bgLayer = {
+    id: 'bgLayer',
+    beforeDatasetsDraw: ((chart, args, plugin) => {
+        const {
+            ctx,
+            chartArea: { top, bottom, left, right, width, height },
+            scales: { x, y }
+        } = chart;
+
+        const bpStages = [
+            {
+                name: 'Low',
+                xEnd: 59,
+                yEnd: 89,
+                color: '#E4C1F9'
+            },
+            {
+                name: 'Ideal',
+                xEnd: 79,
+                yEnd: 119,
+                color: '#68A691'
+            },
+            {
+                name: 'Pre-High',
+                xEnd: 89,
+                yEnd: 139,
+                color: '#C7EF00'
+            },
+            {
+                name: 'High',
+                xEnd: x.max,
+                yEnd: y.max,
+                color: '#FF5E5B'
+            }
+        ];
+
+        bpStages.forEach((stage, index) => {
+            ctx.beginPath();
+            ctx.globalCompositeOperation = 'destination-over';
+            ctx.fillStyle = stage.color
+            ctx.moveTo(x.getPixelForValue(40), y.getPixelForValue(70));
+            ctx.lineTo(x.getPixelForValue(stage.xEnd), y.getPixelForValue(70));
+            ctx.lineTo(x.getPixelForValue(stage.xEnd), y.getPixelForValue(stage.yEnd));
+            ctx.lineTo(x.getPixelForValue(40), y.getPixelForValue(stage.yEnd));
+            ctx.closePath();
+            ctx.fill();
+            ctx.restore();
+            ctx.globalCompositeOperation = 'source-over';
+        });
+    })
+};
+
+new Chart(ctx, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+            label: 'Result',
+            data: [{
+                x: diastolic,
+                y: systolic
+            }],
+            borderWidth: 2,
+            borderColor: '#FFF',
+            backgroundColor: '#007FFF'
+        }]
+    },
+    plugins: [bgLayer],
+    options: {
+        scales: {
+            x: {
+                min: 40,
+                max: 100,
+                title: {
+                    display: true,
+                    text: 'Diastolic'
+                }
+            },
+            y: {
+                min: 70,
+                max: 190,
+                title: {
+                    display: true,
+                    text: 'Systolic'
+                }
+            }
+        }
+    }
+});
---
+++ EndToEndTests/HomePage.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EndToEndTests
+{
+    [TestClass]
+    public class HomePage : PageTest
+    {
+        [TestMethod]
+        public async Task CalculatorPageLoads()
+        {
+            await Page.GotoAsync("http://localhost:5000/");
+            await Expect(Page).ToHaveTitleAsync(new Regex("Calculator - BloodPressure"));
+        }
+
+        [TestMethod]
+
+        public async Task CalculatorHighWorks()
+        {
+            await Page.GotoAsync("http://localhost:5000/");
+
+            await Page.FillAsync("#BP_Systolic", "190");
+            await Page.FillAsync("#BP_Diastolic", "100");
+
+            await Page.ClickAsync("#Submit_Button");
+
+            var result = await Page.EvalOnSelectorAsync<string>("#BP_Category", "el => el.value");
+
+            Assert.AreEqual("High", result);
+        }
+    }
+}
