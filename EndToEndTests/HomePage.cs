e88ccc1fa09b58a8a335829aac77ad4304438068
X00205719 <X00205719@myTUDublin.ie>
2023-12-15 19:29:00 +0000

    feat: change url

--- EndToEndTests/HomePage.cs
+++ EndToEndTests/HomePage.cs
@@ -3,16 +3,22 @@ using System.Threading.Tasks;
 using Microsoft.Playwright;
 using Microsoft.Playwright.MSTest;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
 
 namespace EndToEndTests
 {
     [TestClass]
     public class HomePage : PageTest
     {
+        private readonly string _baseUrl;
+        public HomePage() {
+            var settingsJson = File.ReadAllText("playwrightSettings.json");
+            _baseUrl = JObject.Parse(settingsJson).Value<string>("BaseUrl");
+        }   
         [TestMethod]
         public async Task CalculatorPageLoads()
         {
-            await Page.GotoAsync("http://localhost:5000/");
+            await Page.GotoAsync(_baseUrl);
             await Expect(Page).ToHaveTitleAsync(new Regex("Calculator - BloodPressure"));
         }
 
@@ -20,7 +26,7 @@ namespace EndToEndTests
 
         public async Task CalculatorHighWorks()
         {
-            await Page.GotoAsync("http://localhost:5000/");
+            await Page.GotoAsync(_baseUrl);
 
             await Page.FillAsync("#BP_Systolic", "190");
             await Page.FillAsync("#BP_Diastolic", "100");
