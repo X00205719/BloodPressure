ae3b21f3049db10402af8076cbf1ea12fe07bd37
X00205719 <X00205719@myTUDublin.ie>
2023-12-16 15:37:37 +0000

    fix: refactor JS

--- BPCalculator/wwwroot/js/site.js
+++ BPCalculator/wwwroot/js/site.js
@@ -1,17 +1,22 @@
-﻿const ctx = document.getElementById('myChart');
+﻿const chartCanvas = document.getElementById('myChart');
 const systolicInput = document.getElementById("BP_Systolic");
 const diastolicInput = document.getElementById("BP_Diastolic");
 
-function drawBackgroundLayer(chart, bpStages) {
-    const {
-        ctx,
-        scales: { x, y }
-    } = chart;
+const bpStages = [
+    { name: 'Low', xEnd: 59, yEnd: 89, color: '#E4C1F9' },
+    { name: 'Ideal', xEnd: 79, yEnd: 119, color: '#68A691' },
+    { name: 'Pre-High', xEnd: 89, yEnd: 139, color: '#C7EF00' },
+    { name: 'High', xEnd: 100, yEnd: 190, color: '#FF5E5B' }
+];
 
-    bpStages.forEach((stage, index) => {
+function drawBackgroundLayer(chart) {
+    const { ctx, scales: { x, y } } = chart;
+
+    bpStages.forEach(stage => {
+        ctx.save();
         ctx.beginPath();
         ctx.globalCompositeOperation = 'destination-over';
-        ctx.fillStyle = stage.color
+        ctx.fillStyle = stage.color;
         ctx.moveTo(x.getPixelForValue(40), y.getPixelForValue(70));
         ctx.lineTo(x.getPixelForValue(stage.xEnd), y.getPixelForValue(70));
         ctx.lineTo(x.getPixelForValue(stage.xEnd), y.getPixelForValue(stage.yEnd));
@@ -19,7 +24,6 @@ function drawBackgroundLayer(chart, bpStages) {
         ctx.closePath();
         ctx.fill();
         ctx.restore();
-        ctx.globalCompositeOperation = 'source-over';
     });
 }
 
@@ -27,72 +31,34 @@ function createChart() {
     const systolic = parseFloat(systolicInput.value);
     const diastolic = parseFloat(diastolicInput.value);
 
-    const bpStages = [
-        {
-            name: 'Low',
-            xEnd: 59,
-            yEnd: 89,
-            color: '#E4C1F9'
-        },
-        {
-            name: 'Ideal',
-            xEnd: 79,
-            yEnd: 119,
-            color: '#68A691'
-        },
-        {
-            name: 'Pre-High',
-            xEnd: 89,
-            yEnd: 139,
-            color: '#C7EF00'
-        },
-        {
-            name: 'High',
-            xEnd: 100,
-            yEnd: 190,
-            color: '#FF5E5B'
-        }
-    ];
-
-    const bgLayer = {
+    const bgLayerPlugin = {
         id: 'bgLayer',
-        beforeDatasetsDraw: ((chart, args, plugin) => {
-            drawBackgroundLayer(chart, bpStages);
-        })
+        beforeDatasetsDraw: chart => drawBackgroundLayer(chart)
     };
 
-    new Chart(ctx, {
+    new Chart(chartCanvas, {
         type: 'scatter',
         data: {
             datasets: [{
                 label: 'Result',
-                data: [{
-                    x: diastolic,
-                    y: systolic
-                }],
+                data: [{ x: diastolic, y: systolic }],
                 borderWidth: 2,
                 borderColor: '#FFF',
                 backgroundColor: '#007FFF'
             }]
         },
-        plugins: [bgLayer],
+        plugins: [bgLayerPlugin],
         options: {
             scales: {
                 x: {
                     min: 40,
                     max: 100,
-                    title: {
-                        display: true,
-                        text: 'Diastolic'
-                    }
+                    title: { display: true, text: 'Diastolic' }
                 },
                 y: {
                     min: 70,
                     max: 190,
-                    title: {
-                        display: true,
-                        text: 'Systolic'
-                    }
+                    title: { display: true, text: 'Systolic' }
                 }
             }
         }
